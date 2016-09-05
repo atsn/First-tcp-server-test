402197afcb1329434f7572a8d0e1f64b4c24c30d
anders <anders_stubberup@hotmail.com>
2016-09-05 14:38:51 +0200

    først commit

--- First tcp server test/Program.cs
+++ First tcp server test/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +14,57 @@ namespace First_tcp_server_test
     {
         static void Main(string[] args)
         {
+            Stream stream = null;
+            try
+            {
+                IPAddress ip = Dns.GetHostEntry("localhost").AddressList[0];
+
+                TcpListener server = new TcpListener(ip, 5466);
+
+                server.Start();
+                Console.WriteLine("server startet:  " + server.ToString());
+
+                TcpClient serverclient = server.AcceptTcpClient();
+                Console.WriteLine("client forbundet:  " + server.Server.Connected);
+
+                stream = serverclient.GetStream();
+
+
+                string message = "messege";
+
+                StreamWriter serverStreamWriter = new StreamWriter(stream);
+                StreamReader serverStreamReader = new StreamReader(stream);
+                serverStreamWriter.AutoFlush = true;
+
+                while (message.ToLower() != "stop")
+                {
+                    
+                    message = serverStreamReader.ReadLine();
+                    Console.Clear();
+                    Console.WriteLine("message reshived:  " + message);
+
+                    serverStreamWriter.WriteLine(message.ToUpper() + " DIN MONGOL");
+
+                    Console.WriteLine("messege send:  " + message.ToUpper() + " DIN MONGOL");
+
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                if (stream != null)
+                {
+                    stream.Close();
+                }
+            }
+
+           
         }
     }
 }
