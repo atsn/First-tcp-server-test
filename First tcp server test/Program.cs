0363af67fb18365ff523f580c29cf8f73c088d60
ander <ander@ANDERS-ACER>
2016-09-05 13:15:44 +0200

    Add project files.

---
+++ First tcp server test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_tcp_server_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
